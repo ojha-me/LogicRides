4cd8b4d787f74deb2434f2310b5a771ab9f76926
Dinesh <dineshojha1013@gmail.com>
2025-04-17 22:54:55 +0545

    setup mongodb atlas with mongoose

--- logicrides-backend/server.js
+++ logicrides-backend/server.js
@@ -1,5 +1,10 @@
+// load variables from .env to process.env
+require("dotenv").config();
+
 // import the express library
 const express =  require("express");
+const mongoose = require("mongoose");
+
 const apiRoutes = require("./routes/api");
 
 // create an app ( the express application instance)
@@ -10,6 +15,31 @@ const app = express();
 // process.env.PORT is used in deployed app and we'll use 3000 locally
 const PORT = process.env.PORT || 3000;
 
+// load the db uri
+const dbURI = process.env.MONGODB_URI;
+
+if (!dbURI) {
+    console.error("Database URI is missing in the env. Please provide a valid URI");
+    process.exit(1); // Exit the application if DB connection string is missing
+}
+
+// function to connect to the database
+const connectDB = async () => {
+    try {
+        await mongoose.connect(dbURI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log("Database connection successful");
+    }
+    catch (error) {
+        console.error("Database connection failed", error);
+        process.exit(1);
+    }
+}
+
+connectDB();
+
 // mount the api routes
 // app.use("path", router)
 // any requests starting with /api will be handled by apiRoutes
@@ -18,11 +48,10 @@ app.use("/api", apiRoutes);
 
 app.get('/', (req, res) => {
     res.send('Hello World! LogicRides Backend is Running!'); // Updated message
-  });
+});
 
 
 // start the server and make it listen on the defined port
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
-
